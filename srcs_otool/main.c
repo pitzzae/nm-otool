dca0b25beb8a54a43f176bae6421380b6bace76b
pitzzae <gtorresa@student.42.fr>
2017-06-09 19:46:41 +0200

    Test &

--- srcs_otool/main.c
+++ srcs_otool/main.c
@@ -13,19 +13,49 @@
 #include "libft.h"
 #include "stdio.h"
 
+void	*ft_strcat_chr(char *dst, const char c)
+{
+    size_t	j;
+
+    j = 0;
+    while (dst[j] != '\0')
+        j++;
+    dst[j++] = c;
+    dst[j] = '\0';
+    return (dst);
+}
+
+
+static void		ft_putbase(unsigned long n, char *buf, int base)
+{
+    char		c;
+
+    if (n / base > 0)
+    {
+        ft_putbase((n / base), buf, base);
+        n = n % base;
+    }
+    c = (char) (n < 10 ? '0' : 'a' - 10);
+    c = (char) (c + n);
+    ft_strcat_chr(buf, c);
+}
+
+
 int		main(int ac, char **av)
 {
 	int			fd;
-	char		*buf;
+	char		buf[16];
+    char        r[2048000];
 
 	if (ac < 2)
 		return (0);
 	else
 	{
 		fd = open(av[1], O_RDONLY);
-		while (get_next_line(fd, &buf) == 1)
+		while (read(fd, buf, 1))
 		{
-			printf("%s\n", buf);
+            ft_putbase(buf[0], r, 16);
 		}
+        printf("%s\n", buf);
 	}
 }
