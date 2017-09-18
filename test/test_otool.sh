ebee759575acca3c35a829a2c277348c2c04512d
pitzzae <gtorresa@student.42.fr>
2017-09-18 12:48:32 +0200

    Add test shell script

---
+++ test/test_otool.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FT_OTOOL_PATH="../cmake-build-debug/ft_otool"
+
+run_sys_otool () {
+	otool -t $@
+}
+
+run_ft_otool () {
+    $FT_OTOOL_PATH $@
+}
+
+SYS_OTOOL=$(run_sys_otool $@)
+FT_OTOOL=$(run_ft_otool $@)
+
+echo "/usr/bin/otool -t"
+echo "$SYS_OTOOL"
+
+echo -e "\nft_otool"
+echo "$FT_OTOOL"
+
+echo -e "\ndiff:"
+diff  <(echo "$SYS_OTOOL" ) <(echo "$FT_OTOOL")
