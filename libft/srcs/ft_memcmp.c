1326969dfd544587ef736665ece57edcd1d1aa6e
pitzzae <gtorresa@student.42.fr>
2017-09-19 14:11:15 +0200

    Fix bug on order lst

--- libft/srcs/ft_memcmp.c
+++ libft/srcs/ft_memcmp.c
@@ -14,18 +14,20 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*src1;
-	unsigned char	*src2;
+	unsigned int		i;
+	unsigned char		*ptr1;
+	unsigned char		*ptr2;
 
+	ptr1 = (unsigned char*)s1;
+	ptr2 = (unsigned char*)s2;
+	if (s1 == NULL && s2 == NULL)
+		return (0);
 	i = 0;
-	src1 = (unsigned char *)s1;
-	src2 = (unsigned char *)s2;
-	while (i < n)
+	while (ptr1[i] == ptr2[i] && i <= n)
 	{
-		if (src1[i] != src2[i])
-			return (((int)src1[i] - (int)src2[i]));
 		i++;
+		//if (i == (unsigned int)n || (ptr1[i] == '\0' && ptr2[i] == '\0'))
+		//	return (0);
 	}
-	return (0);
+	return (ptr1[i] - ptr2[i]);
 }
--- srcs_nm/print_symbol_table.c
+++ srcs_nm/print_symbol_table.c
@@ -12,7 +12,7 @@
 
 #include "nmotool.h"
 
-static void	print_ptr_address(unsigned long n, char *buff, boolean_t x64)
+static void	print_ptr_addr(unsigned long n, char *buff, boolean_t x64)
 {
 	char 	tmp_address[16];
 	char	address[16];
@@ -37,15 +37,52 @@ static void	print_ptr_address(unsigned long n, char *buff, boolean_t x64)
 	ft_strcat(buff, address);
 }
 
-static void	add_to_lst(t_list **lst, t_symbol sym)
+static void		fuc_is_public(t_list **lst, t_symbol *sym)
 {
-	if (sym.name[0] == '_' || ft_strcmp(sym.name, "dyld_stub_binder") == 0)
-		ft_lstadd(lst, ft_lstnew(&sym, sizeof(sym)));
+	t_list			*l;
+	t_symbol		*s;
+
+	l = *lst;
+	while (l)
+	{
+		s = ((t_symbol*)(l->content));
+		if (s && ft_strcmp(s->name, sym->name) == 0)
+		{
+			s->type = 'T';
+			*s->address = *sym->address;
+			return;
+		}
+		l = l->next;
+	}
+	ft_lstadd(lst, ft_lstnew(sym, sizeof(*sym)));
+}
+
+static size_t	add_line_to_lst(uint8_t n_type, uint32_t f_type,
+								 uint8_t n_sect, t_symbol *sym)
+{
+	int 			i;
+
+	i = 0;
+	if ((n_type == 0x24 || n_type == 0x0f) && n_sect ==	0x01 && (i = 1))
+	{
+		sym->type = 'T';
+		if (f_type == MH_DYLIB)
+			sym->type = 't';
+	}
+	else if (n_type == 0x01 && n_sect == 0x00 && (i = 1))
+		sym->type = 'U';
+	else if (n_type == 0x26 && (n_sect == 0x0a || n_sect == 0x09) && (i = 1))
+		sym->type = 'd';
+	else if (n_type == 0x0e && n_sect == 0x03 && (i = 1))
+		sym->type = 'd';
+	else if (f_type != MH_DYLIB && n_type == 0x0e && n_sect == 0x01 && (i = 1))
+		sym->type = 'T';
+	return (i);
 }
 
 t_list		*print_symbol_table_86(t_head *head, char *ptr)
 {
-	int				i;
+	size_t			i;
 	t_list			*lst;
 	t_symbol		sym;
 
@@ -56,16 +93,13 @@ t_list		*print_symbol_table_86(t_head *head, char *ptr)
 	{
 		ft_bzero(sym.address, 17);
 		if (head->nlist32[i].n_value > 0)
-			print_ptr_address(head->nlist32[i].n_value, sym.address, head->is_x64);
+			print_ptr_addr(head->nlist32[i].n_value, sym.address, head->is_x64);
 		else
 			ft_strcat(sym.address, "        ");
-		if ((head->nlist32[i].n_type & N_TYPE) == 14)
-			sym.type = 'T';
-		else
-			sym.type = 'U';
-		sym.name = ft_strdup(ptr + head->sym->stroff +
-				 head->nlist32[i].n_un.n_strx);
-		add_to_lst(&lst, sym);
+		sym.name = ptr + head->sym->stroff + head->nlist32[i].n_un.n_strx;
+		if (add_line_to_lst(head->nlist32[i].n_type, head->mach32->filetype,
+							head->nlist32[i].n_sect, &sym))
+			fuc_is_public(&lst, &sym);
 		i++;
 	}
 	return (lst);
@@ -73,7 +107,7 @@ t_list		*print_symbol_table_86(t_head *head, char *ptr)
 
 t_list		*print_symbol_table_64(t_head *head, char *ptr)
 {
-	int				i;
+	size_t			i;
 	t_list			*lst;
 	t_symbol		sym;
 
@@ -84,15 +118,15 @@ t_list		*print_symbol_table_64(t_head *head, char *ptr)
 	{
 		ft_bzero(sym.address, 17);
 		if (head->nlist64[i].n_value > 0)
-			print_ptr_address(head->nlist64[i].n_value, sym.address, head->is_x64);
+			print_ptr_addr(head->nlist64[i].n_value, sym.address, head->is_x64);
 		else
 			ft_strcat(sym.address, "                ");
-		if ((head->nlist64[i].n_type & N_TYPE) == 14)
-			sym.type = 'T';
-		else
-			sym.type = 'U';
 		sym.name = ptr + head->sym->stroff + head->nlist64[i].n_un.n_strx;
-		add_to_lst(&lst, sym);
+		if (ft_strcmp(sym.name, "_read_fd.buf") == 0)
+			sym.name = sym.name;
+		if (add_line_to_lst(head->nlist64[i].n_type, head->mach64->filetype,
+							head->nlist64[i].n_sect, &sym))
+			fuc_is_public(&lst, &sym);
 		i++;
 	}
 	return (lst);
