bb48fb3e4292b19fc541a9d3a92b24164a83c4cb
pitzzae <gtorresa@student.42.fr>
2017-09-20 14:19:43 +0200

    Add script global test

---
+++ test/test_nmotool.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+FT_NM_PATH="../ft_nm"
+FT_OTOOL_PATH="../ft_otool"
+
+run_sys_nm () {
+	nm $@
+}
+
+run_ft_nm () {
+    $FT_NM_PATH $@
+}
+
+run_sys_otool () {
+	otool -t $@
+}
+
+run_ft_otool () {
+    $FT_OTOOL_PATH $@
+}
+
+test_diff_output () {
+    echo "Test $@"
+    echo "nm_diff:"
+    diff  <(echo $(run_sys_nm "$@") ) <(echo $(run_ft_nm "$@"))
+    echo -e "end_nm_diff\n"
+    echo "otool_diff:"
+    diff  <(echo $(run_sys_otool "$@") ) <(echo $(run_ft_otool "$@"))
+    echo -e "end_otool_diff\n"
+}
+
+if [ $1 ]
+then
+    test_diff_output $@
+else
+    test_diff_output "/usr/bin/time"
+    test_diff_output "./libft_malloc.dylib"
+    test_diff_output "../libft/libft.a"
+    test_diff_output "../ft_nm"
+    test_diff_output "../ft_otool"
+    test_diff_output "./dst/*"
+    test_diff_output "./test_nmotool.sh"
+    test_diff_output "./wrong_path"
+    test_diff_output "./dst"
+fi
