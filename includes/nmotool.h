9651e0f0d550defdf0614521fb855829b6e9e848
pitzzae <gtorresa@student.42.fr>
2017-10-18 13:47:21 +0200

    fixed unused files

--- includes/nmotool.h
+++ includes/nmotool.h
@@ -14,7 +14,6 @@
 # define FT_NMOTOOL_H
 # define S_X64 "                "
 # define S_X86 "        "
-# define I(p)		(int)p
 
 # include <stdio.h>
 # include <libft.h>
@@ -26,23 +25,6 @@ typedef struct		s_symbol
 	char						type;
 	char						*name;
 }					t_symbol;
-
-typedef struct		s_libar
-{
-	struct ranlib				*lib;
-	struct ar_hdr				*ar;
-	char						*start;
-	unsigned int				st_len;
-	unsigned int				arr_len;
-	char						*str;
-}					t_libar;
-
-typedef struct		s_seg_com
-{
-	unsigned char				text;
-	unsigned char				data;
-	unsigned char				bss;
-}					t_seg_com;
 
 typedef struct		s_seg_x64
 {
@@ -66,37 +48,7 @@ typedef struct		s_sect
 	char						*sect_text;
 	t_seg_x64					x64;
 	t_seg_x86					x86;
-	t_seg_com					segcom;
 }					t_sect;
-
-typedef struct		s_head
-{
-	struct mach_header			*mach32;
-	struct mach_header_64		*mach64;
-	struct fat_arch				*arch32;
-	struct fat_arch				*arch64;
-	struct load_command			*lc;
-	struct symtab_command		*sym;
-	struct nlist_64				*nlist64;
-	struct nlist				*nlist32;
-	boolean_t					is_x64;
-	t_sect						sect;
-}					t_head;
-
-typedef struct		s_bin
-{
-	int							fd;
-	struct stat					st;
-	void						*ptr;
-	t_head						head;
-}					t_bin;
-
-void				parse_argv(t_bin *bin, int ac, char **av);
-void				get_type_file(t_bin *bin, char *filename, char *name);
-void				find_symbol_table(char *ptr, t_head *header);
-void				ft_putargv_error(char *name, char *source, char *msg);
-size_t				parse_universel_binary(t_bin *bin);
-void				get_segment_command(t_head *head);
 
 void				find_section_64(t_file *bin, t_sect *s);
 void				find_section_32(t_file *bin, t_sect *s);
