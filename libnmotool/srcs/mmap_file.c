3459a8d41806eb7c0774ae846a60532ac8f3be83
pitzzae <gtorresa@student.42.fr>
2017-10-19 16:06:41 +0200

    Fix bug on read dir

--- libnmotool/srcs/mmap_file.c
+++ libnmotool/srcs/mmap_file.c
@@ -6,7 +6,7 @@
 /*   By: gtorresa <gtorresa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/17 16:21:15 by gtorresa          #+#    #+#             */
-/*   Updated: 2017/10/17 16:21:18 by gtorresa         ###   ########.fr       */
+/*   Updated: 2017/10/19 16:06:22 by gtorresa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,19 @@ static void	ft_putargv_error(t_file *bin, char *msg)
 	ft_putendl_fd(msg, 2);
 }
 
-void 		mmap_file(t_file *bin, char *file)
+void		mmap_file(t_file *bin, char *file)
 {
 	bin->ptr = NULL;
 	bin->filename = file;
 	bin->fd = open(bin->filename, O_RDONLY);
 	if (bin->fd > 2 && !fstat(bin->fd, &bin->st))
-		bin->ptr = mmap(0, (size_t)bin->st.st_size,
+	{
+		if (bin->st.st_mode & S_IFDIR)
+			ft_putargv_error(bin, ": Is a directory.");
+		else
+			bin->ptr = mmap(0, (size_t)bin->st.st_size,
 						PROT_READ, MAP_PRIVATE, bin->fd, 0);
+	}
 	else
 		ft_putargv_error(bin, ": No such file or directory.");
 	close(bin->fd);
