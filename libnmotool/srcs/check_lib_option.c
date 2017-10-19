116a4012dccbf1ac5e84b7db3db8b6042eae5d27
pitzzae <gtorresa@student.42.fr>
2017-10-19 15:08:16 +0200

    Fix bug on fat_header select [Result bin 1065/1065], [Result lib 277/281]

--- libnmotool/srcs/check_lib_option.c
+++ libnmotool/srcs/check_lib_option.c
@@ -6,7 +6,7 @@
 /*   By: gtorresa <gtorresa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/18 15:22:15 by gtorresa          #+#    #+#             */
-/*   Updated: 2017/10/19 14:47:38 by gtorresa         ###   ########.fr       */
+/*   Updated: 2017/10/19 15:07:31 by gtorresa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ static int	fat_is_select(t_file *bin)
 	j = 1;
 	while (i < bin->nfat_arch)
 	{
-		if ((!bin->dump->is_64 && bin->fat_l[i]) || bin->is_print)
+		if ((!bin->dump->is_64 && bin->fat_l[i]))
+			j = 0;
+		if (bin->is_print)
 			j = 0;
 		i++;
 	}
@@ -36,6 +38,7 @@ int			check_lib_option(t_file *bin)
 		i = fat_is_select(bin);
 	else
 		i = 1;
-	bin->is_print = i;
+	if (i)
+		bin->is_print = i;
 	return (i);
 }
